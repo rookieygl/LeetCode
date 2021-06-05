dcbd8241e1925124a7b065b512eb67e5ff867d02
杨纲岭 <yanggangling@xin.com>
2021-06-05 23:47:31 +0800

    移除链表元素

--- leet_code/lt_tiku/160_getIntersectionNode.py
+++ leet_code/lt_tiku/160_getIntersectionNode.py
@@ -11,17 +11,39 @@ class ListNode(object):
 
 
 class Solution(object):
-    def getIntersectionNode(self, headA, headB):
-        return True
+    # 哈希表
+    def getIntersectionNode_hash(self, headA, headB):
+        nodeSet = set()
+        while headA:
+            nodeSet.add(headA)
+            headA = headA.next
+
+        while headB:
+            if headB in nodeSet:
+                return headB
+            headB = headB.next
+        return None
+
+    # 双指针 性能更好
+    def getIntersectionNode_doblePoint(self, headA, headB):
+        if not headA or not headB:
+            return None
+        # pa pb的交换 不影响head
+        pa, pb = headA, headB
+        while pa != pb:
+            pa = headB if pa == None else pa.next
+            pb = headA if pb == None else pb.next
+        return pa
 
 
 if __name__ == '__main__':
     node4 = ListNode(4, None)
-    node3 = ListNode(3, node4)
-    node2 = ListNode(2, node3)
+    headC = ListNode(3, node4)
+
+    node2 = ListNode(2, headC)
     headA = ListNode(1, node2)
 
-    headB = ListNode(5, node3)
+    headB = ListNode(5, headC)
     solution = Solution()
-    fun = solution.getIntersectionNode(headA, headB)
-    print(fun)
+    fun = solution.getIntersectionNode_doblePoint(headA, headB)
+    print(fun.val)
