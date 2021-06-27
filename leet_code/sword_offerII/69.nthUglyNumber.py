91c9aab4823263906eed2fb5afef4a9bb39c0a23
杨纲岭 <yanggangling@xin.com>
2021-06-27 12:50:52 +0800

    6月21

---
+++ leet_code/sword_offerII/69.nthUglyNumber.py
@@ -0,0 +1,13 @@
+"""
+"""
+
+
+class Solution(object):
+    def nthUglyNumber(self):
+        return True
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    fun = solution.nthUglyNumber()
+    print(fun)
